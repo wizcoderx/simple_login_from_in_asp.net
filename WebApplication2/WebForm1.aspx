db253d949e7d0168f5f3774c8ebfa49510241e3e
admin <admin@DESKTOP-1BAQBB9>
2024-04-08 15:09:35 +0530

    Add project files.

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: 'Times New Roman', Times, serif; background-color: #00FFFF">
+            LOGIN<br />
+&nbsp;&nbsp;
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            USERNAME<br />
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            PASSWORD<br />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="login" />
+&nbsp;<br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = TextBox1.Text;
+            string password = TextBox2.Text;
+
+            //estabhlish a connection in sql
+            string connectionString = @"Data Source=DESKTOP-1BAQBB9\SQLEXPRESS;Initial Catalog=web_application;Integrated Security=True;TrustServerCertificate=true;";
+
+
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM NEW_USER WHERE USERNAME = @Username AND PASSWORD = @Password";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Username", username);
+                command.Parameters.AddWithValue("@Password", password);
+                int count = (int)command.ExecuteScalar();
+
+                //close connection
+                connection.Close();
+
+                if (count > 0) {
+                    Response.Redirect("https://initiate.alphacoders.com/images/102/cropped-1920-1080-1021182.jpg?3225");
+                }
+
+                else
+                {
+                    //display and error
+                    Label1.Text = "Invalid credentials";
+                }
+            }
+        }
+    }
+}
